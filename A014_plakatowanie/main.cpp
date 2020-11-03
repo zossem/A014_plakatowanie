f43e2ab8f7651daea26d6af7c2f8ed010f687feb
zossem <71115692+zossem@users.noreply.github.com>
2020-11-03 17:42:45 +0100

    Uzycie stosu znacznie przyspiesza rozwiązanie

--- A014_plakatowanie/main.cpp
+++ A014_plakatowanie/main.cpp
@@ -1,77 +1,38 @@
 #include <iostream>
-#include<algorithm>
+#include<stack>
 
 using namespace std;
-
-struct blok
-{
-    long numer;
-    long wysokosc;
-};
-
-bool jak(blok &a, blok b)
-{
-    if(a.wysokosc<b.wysokosc)
-        return true;
-    else if(a.wysokosc==b.wysokosc && a.numer<b.numer)
-        return true;
-    else
-        return false;
-}
 
 int main()
 {
     long wysokosc, dlugosc;
-    long i, j, n, odp;
-    blok *szeregowiec;
-    long pomocny_szeregowiec[250000];
-    bool a;
+    long i, n, odp=0;
+    stack<long> szeregowiec;
 
     scanf("%ld", &n);
-    odp=n;
-
-    if(n>0)
-    {
-        szeregowiec = new blok[n];
-    }
-    if(szeregowiec == NULL)
-    {
-        return 1;
-    }
 
     for(i=0;i<n;i++)
     {
         scanf("%ld", &dlugosc);
         scanf("%ld", &wysokosc);
-        szeregowiec[i].numer=i;
-        szeregowiec[i].wysokosc=wysokosc;
-        pomocny_szeregowiec[i]=wysokosc;
-    }
 
-    sort(szeregowiec, szeregowiec+n, jak);
 
-    for(i=1;i<n;i++)
-    {
-        if(szeregowiec[i].wysokosc == szeregowiec[i-1].wysokosc)
+        while(!(szeregowiec.empty()) && szeregowiec.top()>wysokosc)
         {
-            a=true;
-            for(j=((szeregowiec[i-1].numer)+1); j<szeregowiec[i].numer; j++)
-            {
-                if(pomocny_szeregowiec[j]<szeregowiec[i].wysokosc)
-                    {
-                        a=false;
-                        break;
-                    }
-            }
-            if(a == true)
-                odp--;
+            szeregowiec.pop();
+            odp++;
+        }
+
+
+        if(szeregowiec.empty() || wysokosc > szeregowiec.top())
+        {
+            szeregowiec.push(wysokosc);
         }
     }
 
-    printf("%ld", odp);
+    odp+=szeregowiec.size();
 
-    delete[] szeregowiec;
-    szeregowiec=NULL;
+    printf("%ld", odp);
 
     return 0;
 }
